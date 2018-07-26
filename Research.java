30f20f69a61e3d1f42146a524d11b295ad9286bd
Leiland <leiland12@gmail.com>
2018-07-25 17:38:22 -0700

    Research

---
+++ Research.java
@@ -0,0 +1,88 @@
+package ResearchWK5;
+
+public class Research {
+
+	/*
+	 * 		Read 5 online articles about Object Oriented Programming with Java
+	 * 		Summarize what you learned in each article
+	 * 		Include the link to the article
+	 * 		Research and write about the differences between checked and unchecked exceptions
+	 */
+	
+	/*
+	 * 1) 	https://javatutorial.net/java-oop
+	 * 		The article explains the importance of OOP in the Java language and belabors the point
+	 * 		about even primitive data types having an Object representation (ie. Integer for Int and 
+	 * 		Double for double). Another useful bite of information was their warning that most job
+	 * 		interviews will contain one or more questions about OOP - so pay attention! The remainder
+	 * 		of the article is a summary of what OOP means - giving the analogy of a car being the total
+	 * 		collection and integration of many objects into one. The main paradigm behind OOP is that 
+	 * 		the objects contain data in form of attributes or 'fields' - and the data is manipulated
+	 * 		through different sets of procedures called 'methods'. The 4 pillars of OOP are also mentioned
+	 * 		and defined as Encapsulation, Inheritance, Abstraction, and Polymorphism. 
+	 * 
+	 * 2)	https://www.edureka.co/blog/object-oriented-programming/
+	 * 		This article jumps right into discussing more specifically how in a Java OOP paradigm
+	 * 		it is about creating objects in Java Classes and learning how to make them interact. 
+	 * 		It straightway jumps into defining the pillars of OOP starting with Inheritance. I will
+	 * 		summarize their definitions of the pillars:
+	 * 			Inheritance = the idea that the properties of one class can be reused or inherited 
+	 * 			by another. They give the analogy of a father and son and identify 4 types of inheritance.
+	 * 			Single inheritance, multilevel, hierarchical, and hybrid. 'extends' is the syntax. 
+	 * 			Encapsulation = Binding your data together and hiding the information you don't want seen
+	 * 			or changed. They give a picture of a pill.
+	 * 			Abstraction = a way to simplify the outcome of a potentially complex code network. Two main
+	 * 			ways to go about this are through an Abstract Class and through an Interface. In an 
+	 * 			Abstract Class, it has to be inherited and cannot be instantiated. They describe an Interface
+	 * 			as a blueprint of a class or a collection of abstract methods and static constants where each
+	 * 			method is public - but it does not contain constructors. 
+	 * 			Polymorphism = essentially like the phonetic spelling would suggest it is a way for 
+	 * 			an interface or a method to have multiple implementations, or 'many' 'forms'. 
+	 * 
+	 * 3)	https://docs.oracle.com/javase/tutorial/java/concepts/
+	 * 		This was a short article summarizing much of what I had already gleaned from class lectures
+	 * 		and what I've read now previously and provided links to more in depth articles about each of
+	 * 		the definitions presented. I will still record some of their definitions here so I can 
+	 * 		better retain the definitions of these basic/fundamental OOP terms. An Object is a code version 
+	 * 		of actual objects in real life - the Class is the blueprint by which the objects are created -
+	 * 		Inheritance is the mechanism by which classes can inherit traits and behavior from their superclasses - 
+	 * 		Interface provides a way for the classes to be contracted to the outside world. A Package is a namespace
+	 * 		for organizing classes and interfaces in an organized manner. 
+	 * 
+	 * 4)	https://stackify.com/oops-concepts-in-java/
+	 * 		Another article that provides great definitions and examples for the 4 pillars of OOP I have now 
+	 * 		previously written about. I will mention here that I liked their clear explanation for two examples 
+	 * 		of polymorphism - namely; method overriding and method overloading. In method overriding, the child
+	 * 		class can use the OOP polymorphism concept to override a method in the parent class. Method overloading
+	 * 		is when a single method may function for many different types of applications based on the information passed in.
+	 * 		These weren't as explicitly stated in the previous articles that I can recall.  
+	 * 
+	 * 5) 	https://www.oracle.com/technetwork/java/oo-140949.html
+	 * 		Right off the bat in this article I loved the OOP initial definition they give, "Object-oriented programming 
+	 * 		is a method of programming based on a hierarchy of classes, and well-defined and cooperating objects". The 
+	 * 		article then provides more in depth sections for topics we've already covered, Classes, Objects, Inheritance,
+	 * 		and Polymorphism. There were two other sections mentioned that used new terminology to me - those sections were 
+	 * 		titled "Well-Defined Boundaries and Cooperation" and "Data Access Levels".  The latter deals with making your 
+	 * 		methods and classes public or private and the first dealt with options for defining a user interface for displaying
+	 * 		the data in manipulation. 
+	 * 
+	 * Checked and Unchecked Exceptions 
+	 * 		 
+	 * 		Checked Exceptions are errors in your code that are checked at the time compilation. The way to avoid these
+	 * 		are to use a try-catch block 
+	 * 				try	{
+	 *			     //statements that may cause an exception
+	 *				}
+	 *				catch (exception(type) e(object))‏	{
+	 *				 //error handling code
+	 *				}		
+	 *		or it should be declared in the signature with the throws keyword. 
+	 *
+	 * 		Unchecked Exceptions are not checked at compilation and are all direct subclasses of RuntimeException class. 
+	 * 
+	 * 		https://beginnersbook.com/2013/04/java-checked-unchecked-exceptions-with-examples/
+	 * 		https://www.geeksforgeeks.org/checked-vs-unchecked-exceptions-in-java/
+	 * 
+	 */
+	
+}
